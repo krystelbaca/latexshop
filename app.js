a0fd34593a36a6ce89a3edbf2a967774f09f11c5
Krystel Baca <salayandia.k@gmail.com>
2016-05-14 01:46:25 -0600

    Se agrego template, se modifico archivo index, se configura motor de plantillas ejs

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose  = require('mongoose');
 
+
+
+
 var app = express();
+
 var port = process.env.PORT||8080;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.set('view options', {layout:false});
-app.use(express.static(__dirname + '/views'));
-app.get('/', function(req, res){
-	res.render('index.html');
-})
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+	res.render('index');
+});
 
 app.listen(port, function(){
 	console.log("Escuchando en el puerto " + port);
-})
+});
+
